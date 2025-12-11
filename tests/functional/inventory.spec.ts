3c6d2d089630e577299550a96d1be44bdc89b033
Fadida Junaedy <fadidajunaedy@gmail.com>
2025-12-11 13:01:55 +0700

    ADDING SORT TEST SUITE

--- tests/functional/inventory.spec.ts
+++ tests/functional/inventory.spec.ts
@@ -4,40 +4,66 @@ import { InventoryPage } from "../../pages/InventoryPage";
 
 test.beforeEach(async ({ page }) => {
   const loginPage = new LoginPage(page);
+
   await loginPage.goTo();
   await loginPage.login('standard_user', 'secret_sauce');
-
   await expect(page).toHaveURL(/.*inventory.html/);
   await expect(page.locator('.title')).toHaveText('Products');
 });
 
 test('User should be able to add item to cart', async({ page }) => {
   const inventoryPage = new InventoryPage(page);
-  await inventoryPage.addItemToCart("Sauce Labs Backpack");
 
+  await inventoryPage.addItemToCart("Sauce Labs Backpack");
   await expect(inventoryPage.cartBadge).toHaveText('1');
 });
 
 test('User should be able to click item', async({ page }) => {
   const inventoryPage = new InventoryPage(page);
-  await inventoryPage.goToDetailItem("Sauce Labs Backpack");
 
+  await inventoryPage.goToDetailItem("Sauce Labs Backpack");
   await expect(page).toHaveURL(/.*inventory-item.html/);
   await expect(page.locator('.inventory_details_name')).toHaveText("Sauce Labs Backpack");
 });
 
 test('User should be able to sort items by Name (A to Z)', async ({ page }) => {
+  const inventoryPage = new InventoryPage(page);
+  const initialOrderItem: string[] = await page.locator('.inventory_item_name').allTextContents();
+  const ascendingOrderItem: string[] = [...initialOrderItem].sort();
 
+  await inventoryPage.sortItems('az');
+  const orderAfterSorted: string[] = await page.locator('.inventory_item_name').allTextContents();
+  expect(orderAfterSorted).toEqual(ascendingOrderItem);
 });
 
 test('User should be able to sort items by Name (Z to A)', async ({ page }) => {
+  const inventoryPage = new InventoryPage(page);
+  const initialOrderItem: string[] = await page.locator('.inventory_item_name').allTextContents();
+  const descendingOrderItem: string[] = [...initialOrderItem].sort().reverse();
 
+  await inventoryPage.sortItems('za');
+  const orderAfterSorted: string[] = await page.locator('.inventory_item_name').allTextContents();
+  expect(orderAfterSorted).toEqual(descendingOrderItem);
 });
 
 test('User should be able to sort items by Price (low to high)', async ({ page }) => {
+  const inventoryPage = new InventoryPage(page);
+  const initialOrderItem: string[] = await page.locator('.inventory_item_price').allTextContents();
+  const lowToHighOrderItem: number[] = [...initialOrderItem].map(price => parseFloat(price.replace('$', ''))).sort((a, b) => a - b);
 
+  await inventoryPage.sortItems('lohi');
+  const orderAfterSorted = await page.locator('.inventory_item_price').allTextContents();
+  const parsedOrderAfterSorted = [...orderAfterSorted].map(price => parseFloat(price.replace('$', '')));
+  expect(parsedOrderAfterSorted).toEqual(lowToHighOrderItem);
 });
 
 test('User should be able to sort items by Price (high to low)', async ({ page }) => {
+  const inventoryPage = new InventoryPage(page);
+  const initialOrderItem: string[] = await page.locator('.inventory_item_price').allTextContents();
+  const highToLowOrderItem: number[] = [...initialOrderItem].map(price => parseFloat(price.replace('$', ''))).sort((a, b) => a - b).reverse();
 
+  await inventoryPage.sortItems('hilo');
+  const orderAfterSorted = await page.locator('.inventory_item_price').allTextContents();
+  const parsedOrderAfterSorted = [...orderAfterSorted].map(price => parseFloat(price.replace('$', '')));
+  expect(parsedOrderAfterSorted).toEqual(highToLowOrderItem);
 });
