f25273319e4f73b0c4fccf8a6c78ca9867532080
Fadida Junaedy <fadidajunaedy@gmail.com>
2025-12-14 15:04:41 +0700

    CREATING CHECKOUT STEP TWO TEST SUITES

---
+++ tests/functional/checkout-step-two.spec.ts
@@ -0,0 +1,51 @@
+import { test, expect } from "@playwright/test";
+import { LoginPage } from "../../pages/LoginPage";
+import { InventoryPage } from "../../pages/InventoryPage";
+import { CartPage } from "../../pages/CartPage";
+import { CheckoutStepOnePage } from "../../pages/CheckoutStepOnePage";
+import { CheckoutStepTwoPage } from "../../pages/CheckoutStepTwoPage";
+
+test.beforeEach(async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  await loginPage.goTo();
+  await loginPage.login('standard_user', 'secret_sauce');
+  await expect(page).toHaveURL(/.*inventory.html/);
+  await expect(page.locator('.title')).toHaveText('Products');
+
+  const inventoryPage = new InventoryPage(page);
+  await inventoryPage.addItemToCart('Sauce Labs Backpack');
+  await inventoryPage.navbar.goToCart();
+  await expect(page).toHaveURL(/.*cart.html/);
+  await expect(page.locator('.title')).toHaveText('Your Cart');
+
+  const cartPage = new CartPage(page);
+  await cartPage.proceedToCheckout();
+  await expect(page).toHaveURL(/.*checkout-step-one.html/);
+  await expect(page.locator('.title')).toHaveText('Checkout: Your Information');
+
+  const checkoutStepOnePage = new CheckoutStepOnePage(page);
+  await checkoutStepOnePage.proceedToCheckout('fadida', 'junaedy', "13740");
+  await expect(page).toHaveURL(/.*checkout-step-two.html/);
+  await expect(page.locator('.title')).toHaveText('Checkout: Overview');
+});
+
+test('User should be able to cancel checkout', async ({ page }) => {
+  const checkoutStepTwoPage = new CheckoutStepTwoPage(page);
+  await checkoutStepTwoPage.cancelCheckout();
+
+  await expect(page).toHaveURL(/.*inventory.html/);
+  await expect(page.locator('.title')).toHaveText('Products');
+});
+
+test('User should be able to finish to checkout', async ({ page }) => {
+  const checkoutStepTwoPage = new CheckoutStepTwoPage(page);
+  const subTotal = await checkoutStepTwoPage.getSubTotal();
+  const tax = await checkoutStepTwoPage.getTax();
+  const total = await checkoutStepTwoPage.getTotal();
+
+  expect(subTotal + tax).toEqual(total);
+
+  await checkoutStepTwoPage.finishCheckout();
+  await expect(page).toHaveURL(/.*checkout-complete.html/);
+  await expect(page.locator('.title')).toHaveText('Checkout: Complete!');
+});
