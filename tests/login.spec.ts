1faaa37c0a8f76f5a4812fa6326ba817208c1d38
Fadida Junaedy <fadidajunaedy@gmail.com>
2025-12-14 15:23:09 +0700

    ADDING NEGATIVE TEST SUITE FOR LOGIN

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -10,4 +10,14 @@ test('User should be able login with valid credentials', async ({ page }) => {
   await expect(page.locator('.title')).toHaveText('Products');
 });
 
+test('User should not be able login with invalid credentials', async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  await loginPage.goTo();
+  await expect(page.locator('[data-test="error"]')).toBeHidden();
+
+  await loginPage.login('random_user', 'random_password');
+  await expect(page.locator('[data-test="error"]')).toBeVisible();
+  await expect(page.locator('[data-test="error"]')).toContainText('Username and password do not match any user in this service');
+});
+
 
